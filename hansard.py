e30c5e4478b3939cc8b037b750e14728ba1022b9
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-02-17 14:44:43 +1100

    Update hansard.py

--- hansard.py
+++ hansard.py
@@ -1,7 +1,7 @@
-# __import__('pysqlite3')
+import(‘pysqlite3’)
 import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-import pysqlite3-binary
+sys.modules[‘sqlite3’] = sys.modules.pop(‘pysqlite3’)
+# import pysqlite3-binary
 
 import streamlit as st
 import ollama
