e18e53edc9bff9a8cc5f230ee7b4f81cd0856e5f
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-02-17 14:23:40 +1100

    Update hansard.py

--- hansard.py
+++ hansard.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import ollama
-import pysqlite3
-import sys
-sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 import chromadb
 # from chromadb.utils import embedding_functions
 # from streamlit_chromadb_connection.chromadb_connection import ChromadbConnection
