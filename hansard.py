3c7d959e13544a4b3f94658e81392498d49732c1
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-02-17 15:06:21 +1100

    Update hansard.py

--- hansard.py
+++ hansard.py
@@ -3,11 +3,11 @@ import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
-# import ollama
+import ollama
 import chromadb
-# from chromadb.utils import embedding_functions
+from chromadb.utils import embedding_functions
 # from streamlit_chromadb_connection.chromadb_connection import ChromadbConnection
-'''
+
 CHROMA_DATA_PATH = "chroma_data/"
 EMBED_MODEL = "all-MiniLM-L6-v2"
 COLLECTION_NAME = "demo_docs"
@@ -18,5 +18,5 @@ st.write("after ChromaDB client create")
 embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(
      model_name=EMBED_MODEL
  )
-'''
+
 st.write("after embedding function create")
