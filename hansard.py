70230e5a27b6d9e51a2eaa0f95e2bae1b466fd1a
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-02-17 15:17:39 +1100

    Update hansard.py

--- hansard.py
+++ hansard.py
@@ -2,6 +2,7 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 import streamlit as st
 import ollama
 import chromadb
