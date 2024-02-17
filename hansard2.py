95efff9a5da94d11156f26ebef0a842d1748b963
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-02-17 19:40:19 +1100

    Update hansard2.py

--- hansard2.py
+++ hansard2.py
@@ -1,14 +1,11 @@
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import streamlit as st
-# import ollama
 from openai import OpenAI
 import chromadb
 from chromadb.utils import embedding_functions
-# from streamlit_chromadb_connection.chromadb_connection import ChromadbConnection
 
 CHROMA_DATA_PATH = "chroma_data/"
 EMBED_MODEL = "all-MiniLM-L6-v2"
@@ -21,9 +18,6 @@ embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(
      model_name=EMBED_MODEL
  )
 
-st.write("after embedding function create")
-
-# collection = client.create_collection(
 collection = client.get_or_create_collection(
      name=COLLECTION_NAME,
      embedding_function=embedding_func,
@@ -34,7 +28,6 @@ with open("hansard-utf8.txt") as f:
     hansard = f.read()
 
 text_splitter = RecursiveCharacterTextSplitter(
-#     Set a really small chunk size, just to show.
     chunk_size=500,
     chunk_overlap=20,
     length_function=len,
@@ -42,10 +35,6 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 
 texts = text_splitter.create_documents([hansard])
-st.write(texts[0])
-# print(texts[1])
-# print(texts[2])
-# print(texts[3])
 
 documents = text_splitter.split_text(hansard)[:len(texts)]
 st.write(documents)
@@ -53,13 +42,11 @@ st.write(documents)
 collection.add(
      documents=documents,
      ids=[f"id{i}" for i in range(len(documents))],
-#     metadatas=[{"genre": g} for g in genres]
 )
 
 # number of rows
 st.write(len(collection.get()['documents']))
 
-# prompt = ("What were the key points provided by Ms Jenkins relating to underpayment in the department? How much was the underpayment? For what period of time? What were the actions taken?")
 prompt = ("What are the key questions that Senator Cash asks? What were on notice?")
 
 query_results = collection.query(
@@ -68,9 +55,6 @@ query_results = collection.query(
      include=["documents"],
      n_results=100,
  )
-
-# print(query_results["embeddings"])
-# st.write(query_results["documents"])
 
 augment_query = str(query_results["documents"])
 st.write(augment_query)
@@ -95,24 +79,6 @@ if st.button("Submit to AI", type="primary"):
        max_tokens=1000,
        top_p=1
      )
+     
      st.write(response.choices[0].message.content)
-
-
-'''
-response = ollama.chat(
-    model='llama2',
-    messages=[
-        {
-            "role": "system",
-            "content": "You are a friendly assistant."
-        },
-        {
-            "role": "user",
-            "content": augment_query + " Prompt: " + prompt
-        },
-    ],
-)
-'''
-
-# st.write(response['message']['content'])
 
