4e2f82b83b6fa53b515dde140d4954779a339272
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-02-17 14:15:35 +1100

    Update hansard.py

--- hansard.py
+++ hansard.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import ollama
-import chromadb
-from chromadb.utils import embedding_functions
-
+# import chromadb
+# from chromadb.utils import embedding_functions
+from streamlit_chromadb_connection.chromadb_connection import ChromadbConnection
+'''
 CHROMA_DATA_PATH = "chroma_data/"
 EMBED_MODEL = "all-MiniLM-L6-v2"
 COLLECTION_NAME = "demo_docs"
@@ -13,5 +14,5 @@ st.write("after ChromaDB client create")
 embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(
      model_name=EMBED_MODEL
  )
-
+'''
 st.write("after embedding function create")
