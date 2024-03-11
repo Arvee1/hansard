ec9f40672d6f2b65184e719938aec3c3c8108693
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-03-11 18:06:58 +1100

    Update handsard3.py

--- handsard3.py
+++ handsard3.py
@@ -41,12 +41,6 @@ if st.sidebar.button("Load Hansard into Vector DB if loading the page for the fi
     documents = text_splitter.split_documents(docs)
     vectorstore = Chroma.from_documents(documents, embeddings)
     retriever = vectorstore.as_retriever()
-    # retriever_tool = create_retriever_tool(
-       # retriever,
-       # "handsard_search",
-      # "Search for information about Handsard. For any questions about Handsard, you must use this tool!",
-     # )
-     # tools = [retriever_tool]
     
 if st.button("Submit to DJ Arvee", type="primary"):
      # query_results = collection.query(
@@ -56,10 +50,12 @@ if st.button("Submit to DJ Arvee", type="primary"):
           # n_results=75,
      # )
      # Get the prompt to use - you can modify this!
-      
-     api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
-     tools = WikipediaQueryRun(api_wrapper=api_wrapper)
-
+     retriever_tool = create_retriever_tool(
+        retriever,
+        "handsard_search",
+        "Search for information about Handsard. For any questions about Handsard, you must use this tool!",
+     )
+     tools = [retriever_tool]
      prompt_template = hub.pull("hwchase17/openai-functions-agent")
      llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, api_key=st.secrets["api_key"])
      agent = create_openai_functions_agent(llm, tools, prompt_template)
