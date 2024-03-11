782d8cde6a0d34f7c4c9aee31b4cad5f72a7736d
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-03-11 14:51:01 +1100

    Update handsard3.py

--- handsard3.py
+++ handsard3.py
@@ -12,12 +12,22 @@ from langchain_community.embeddings.sentence_transformer import (
 )
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Chroma
+from langchain.tools.retriever import create_retriever_tool
+from langchain_openai import ChatOpenAI
+from langchain import hub
+from langchain.agents import create_openai_functions_agent
+from langchain.agents import AgentExecutor
+
+retriever_tool = create_retriever_tool(
+    retriever,
+    "handsard_search",
+    "Search for information about Handsard. For any questions about Handsard, you must use this tool!",
+)
+tools = [retriever_tool]
 
 # The UI Part
 st.title("👨‍💻 Wazzup!!!! Let's Chat with the Hansard Senate Estimates for Employment Department (DEWR) - 14 Feb 2024")
-# apikey = st.sidebar.text_area("Please enter enter your API Key.")
 prompt = st.text_area("Please enter what you want to know from the hearing for the Employment Department.")
-
 
 # Load VectorDB
 if st.sidebar.button("Load Hansard into Vector DB if loading the page for the first time.", type="primary"):
@@ -37,27 +47,6 @@ if st.sidebar.button("Load Hansard into Vector DB if loading the page for the fi
      vectorstore = Chroma.from_documents(documents, embeddings)
      retriever = vectorstore.as_retriever()
 
-     # with open("hansardFeb2024.txt") as f:
-         # hansard = f.read()
-         # text_splitter = RecursiveCharacterTextSplitter(
-             # chunk_size=750,
-             # chunk_overlap=50,
-             # length_function=len,
-             # is_separator_regex=False,
-         # )
-    
-     # texts = text_splitter.create_documents([hansard])
-     # documents = text_splitter.split_text(hansard)[:len(texts)]
-    
-     # collection.add(
-          # documents=documents,
-          # ids=[f"id{i}" for i in range(len(documents))],
-     # )
-  
-     # number of rows
-     # st.write(len(collection.get()['documents']))
-     # st.sidebar.write("Hansard Vector DB created. With " + len(collection.get()['documents']) + " rows." )
-
 if st.button("Submit to DJ Arvee", type="primary"):
      # query_results = collection.query(
           # query_texts=[prompt],
@@ -65,27 +54,11 @@ if st.button("Submit to DJ Arvee", type="primary"):
           # include=["documents"],
           # n_results=75,
      # )
-     query_results = retriever.invoke(prompt)
-     augment_query = str(query_results["documents"])
-    
-     client_AI = OpenAI(api_key=st.secrets["api_key"])
-     response = client_AI.chat.completions.create(
-       model="gpt-3.5-turbo",
-       messages=[
-         {
-           "role": "system",
-           "content": "You are a friendly assistant."
-         },
-         {
-           "role": "user",
-           "content": augment_query + " Prompt: " + prompt
-         }
-       ],
-       temperature=0.1,
-       max_tokens=1000,
-       top_p=1
-     )
-     
-     st.write(response.choices[0].message.content)
+     # Get the prompt to use - you can modify this!
+     prompt_template = hub.pull("hwchase17/openai-functions-agent")
+     llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, api_key=st.secrets["api_key"])
+     agent = create_openai_functions_agent(llm, tools, prompt_template)
+     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+     st.write(agent_executor.invoke({"input": prompt}))
 
 
