f1f62859bd3736be79319c5209e6fcc65a39fd53
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-06-13 21:33:59 +1000

    Update hansard2.py

--- hansard2.py
+++ hansard2.py
@@ -66,24 +66,24 @@ if st.button("Submit to DJ Arvee", type="primary"):
      augment_query = str(query_results["documents"])
 
      result_ai = ""
-          # The meta/llama-2-7b-chat model can stream output as it's running.
-          # The meta/meta-llama-3-70b-instruct model can stream output as it's running.
-          for event in replicate.stream(
-              "meta/meta-llama-3-70b-instruct",
-              input={
-                  "top_k": 50,
-                  "top_p": 0.9,
-                  "prompt": "Prompt: " + prompt + " " + augment_query,
-                  "max_tokens": 512,
-                  "min_tokens": 0,
-                  "temperature": 0.6,
-                  "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
-                  "presence_penalty": 1.15,
-                  "frequency_penalty": 0.2
-              },
-          ):
-              result_ai = result_ai + (str(event))
-          st.write(result_ai)
+     # The meta/llama-2-7b-chat model can stream output as it's running.
+     # The meta/meta-llama-3-70b-instruct model can stream output as it's running.
+     for event in replicate.stream(
+         "meta/meta-llama-3-70b-instruct",
+         input={
+             "top_k": 50,
+             "top_p": 0.9,
+             "prompt": "Prompt: " + prompt + " " + augment_query,
+             "max_tokens": 512,
+             "min_tokens": 0,
+             "temperature": 0.6,
+             "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+             "presence_penalty": 1.15,
+             "frequency_penalty": 0.2
+         },
+     ):
+         result_ai = result_ai + (str(event))
+     st.write(result_ai)
 
      # client_AI = OpenAI(api_key=apikey)
      # response = client_AI.chat.completions.create(
